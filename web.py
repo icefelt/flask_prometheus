b96cff4c980727db3e6f56cb8d4c9362ce75b115
Scott Eissfeldt <scott@eissfeldt.com>
2020-06-06 15:11:31 -0700

    fix python sleep undefined error

--- web.py
+++ web.py
@@ -29,8 +29,31 @@ TIMER = Histogram(
     ['endpoint']
 )
 
+import time
+import random
+
 @app.route('/database/')
 def database():
     with TIMER.labels('/database').time():
-        sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(1, 3))
     return '<h1>Completed expensive database operation</h1>'
+
+from prometheus_client import start_http_server, Summary
+import random
+import time
+
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        process_request(random.random())
